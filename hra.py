643d7d70f0e60d4ee76ff20896ee113071e8ca03
NikolasMichalicka <82445527+NikolasMichalicka@users.noreply.github.com>
2021-06-02 22:56:30 +0200

    Update hra.py

--- hra.py
+++ hra.py
@@ -129,7 +129,7 @@ base_font = pygame.font.Font(None, 32)
 user_text = ''
 input_rect = pygame.Rect(345, 200, 140, 32)
 color_active = pygame.Color('lightskyblue3')
-color_passive = pygame.Color('chartreuse4')
+color_passive = pygame.Color('lightblue4')#chartreuse4
 color = color_passive
 
 #Hráč
